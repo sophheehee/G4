21c89b13df5eec9c807f42d227280df099e6f616
Sophie Omar <somar12@saddleback.edu>
2025-11-17 17:38:18 -0800

    delete book files added

---
+++ deleteBook.cpp
@@ -0,0 +1,11 @@
+/***************************
+* Programmer: Sophia Omar
+* Assignment: Serendipity G4
+* Due Date: 11/16/2025
+* Purpose: Delete Book Function
+*****************************/
+#include <iostream>
+#include <vector>
+#include "deleteBook.h"
+
+int main(){}
---
+++ deleteBook.h
@@ -0,0 +1,16 @@
+/***************************
+* Programmer: Sophia Omar
+* Assignment: Serendipity G4
+* Due Date: 11/16/2025
+* Purpose: Delete Book Function Header
+*****************************/
+#ifndef DELETE_BOOK_H
+#define DELETE_BOOK_H
+
+
+
+
+
+
+
+#endif
